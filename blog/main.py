0371203ad3c5f4f7aae97aaf72254e0b61f071f4
jraguilo <johnreyaguilo@gmail.com>
2014-09-06 22:13:32 -0700

    added basic functionality

---
+++ blog/main.py
@@ -0,0 +1,28 @@
+import os
+
+import webapp2
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
+                               
+class BlogHandler(webapp2.RequestHandler):
+    #For reference, *a is arguments, *kw is dictionary
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class MainHandler(BlogHandler):
+    def get(self):
+        self.render('front.html')
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
